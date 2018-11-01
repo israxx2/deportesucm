4588dd949c9cc1fa0366665b8c300f598b863589
zegrim <krlos.erd8@gmail.com>
2018-11-01 15:23:55 -0300

    se agregan los middlewares

---
+++ app/Http/Middleware/MDusuarioAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Contracts\Auth\Guard;
+use auth;
+use Session;
+use Closure;
+
+class MDusuarioAdmin
+{
+    protected $auth;
+
+    public function __construct(Guard $auth){
+      $this->auth = $auth;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($this->auth->user()->tipo !="admin"){
+            Session::flash('message_error','No tiene permisos suficientes para acceder');
+            return redirect()->to('/secre');
+        }
+        return $next($request);
+    }
+}
